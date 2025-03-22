981e904240ebb86e2ed11e3674b6d8ce480dd236
IamDg <180081466+IamDg@users.noreply.github.com>
2025-03-22 17:17:13 +0100

    chore: added aboutLibraries plugin and dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,7 @@ plugins {
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.about.libraries)
 }
 
 android {
@@ -87,6 +88,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
+    implementation(libs.aboutlibraries.compose.m3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

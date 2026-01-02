53ec75427bf69458227e436c429c06404d767cb9
IamDg <iamdg.dev@proton.me>
2026-01-02 19:39:33 +0100

    style: update copyright

--- app/src/main/java/org/librefit/ui/screens/shared/SupportScreen.kt
+++ app/src/main/java/org/librefit/ui/screens/shared/SupportScreen.kt
@@ -1,23 +1,9 @@
 /*
- * Copyright (c) 2025. LibreFit Team
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * Copyright (c) 2025-2026. The LibreFit Contributors
  *
- * This file is part of LibreFit
- *
- * LibreFit is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * LibreFit is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with LibreFit.  If not, see <https://www.gnu.org/licenses/>.
- *
- * LibreFit is subject to additional terms covering author attribution and
- * trademark usage, as found in the accompanying ADDITIONAL_TERMS.md and TRADEMARK_POLICY.md.
+ * LibreFit is subject to additional terms covering author attribution and trademark usage;
+ * see the ADDITIONAL_TERMS.md and TRADEMARK_POLICY.md files in the project root.
  */
 
 package org.librefit.ui.screens.shared

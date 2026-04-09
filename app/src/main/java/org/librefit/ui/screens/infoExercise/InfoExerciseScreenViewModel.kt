0b749fe6851fe9ae3830c48ae936b0aea9362864
IamDg <iamdg.dev@proton.me>
2026-04-09 09:49:19 +0200

    fix: crash when deleting custom exercise #17 - It was caused by a checkNotNull in viewmodel which thrown an error when exercise was deleted (because room could not find any entry so it emitted null). Instead, by using `filterNotNull`, the flow will keep its current value so the screen is allowed to exit.

--- app/src/main/java/org/librefit/ui/screens/infoExercise/InfoExerciseScreenViewModel.kt
+++ app/src/main/java/org/librefit/ui/screens/infoExercise/InfoExerciseScreenViewModel.kt
@@ -20,6 +20,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
@@ -55,7 +56,7 @@ class InfoExerciseScreenViewModel @Inject constructor(
 
     // Keeps track of changes (e.g. the user edits the exercise)
     val uiExerciseDC = datasetRepository.getExerciseFlowFromId(idExerciseDC)
-        .map { checkNotNull(it) {"Invalid `idExerciseDC`: $idExerciseDC" } }
+        .filterNotNull()
         .distinctUntilChanged()
         .stateIn(
             scope = viewModelScope,
